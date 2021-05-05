ffecd3a4fce9f286d9b643de7ed0f78c1e7f977d
sayanmndl21 <sayanmndl21@github.com>
2021-05-05 02:36:15 -0400

    model inference added

--- data_loader.py
+++ data_loader.py
@@ -38,3 +38,153 @@ class FundusDataLoader(torch.utils.data.Dataset):
             image = self.transform(image)
         return image, label
 
+
+#loads oct + vf significant values as well
+class PROBDataLoader(torch.utils.data.Dataset):
+    def __init__(self, csv_path, img_folder, subjective_csv = '/home/vip/sayan-mandal/datasets/obj_criteria/20200623-images_with_subjective.csv', \
+         labeltype = 'mdprob',suspect =False,transform = None):
+        df = pd.read_csv(csv_path, low_memory=False)
+        df_subjective = pd.read_csv(subjective_csv)
+        df_subjective = df_subjective[['maskedid']].drop_duplicates().reset_index(drop = True)
+        df_subjective['with_subjective'] = 1
+
+        df = df.join(df_subjective.set_index('maskedid'), on = 'maskedid')
+        df = df.loc[df.with_subjective.isnull() == True].reset_index(drop = True)
+        self.img_folder = img_folder
+        self.transform = transform
+        self.labeltype = labeltype    
+        self.prob = {'Not Significant': 0,
+                        '< 10%': 1,
+                        '< 5%': 2,
+                        '< 2%' : 3,
+                        '< 1%' : 4,
+                        '< 0.5%' : 5
+                            }
+        if not suspect:
+            self.df = df.loc[df['classification2'] != 'suspect']
+        else:
+            self.df = df
+
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        filename = self.df.iloc[index]["file_jpg"]
+        label = self.prob[self.df.iloc[index][self.labeltype]]
+        image = PIL.Image.open(os.path.join(self.img_folder,filename))
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, label
+
+
+class MDDataLoader(torch.utils.data.Dataset):
+    def __init__(self, csv_path, img_folder, subjective_csv = '/home/vip/sayan-mandal/datasets/obj_criteria/20200623-images_with_subjective.csv', \
+         labeltype = ['md_inf_prob','md_sup_prob'],suspect =False,transform = None):
+        df = pd.read_csv(csv_path, low_memory=False)
+        df_subjective = pd.read_csv(subjective_csv)
+        df_subjective = df_subjective[['maskedid']].drop_duplicates().reset_index(drop = True)
+        df_subjective['with_subjective'] = 1
+
+        df = df.join(df_subjective.set_index('maskedid'), on = 'maskedid')
+        df = df.loc[df.with_subjective.isnull() == True].reset_index(drop = True)
+        self.img_folder = img_folder
+        self.transform = transform
+        self.labeltype = labeltype    
+        self.prob = {'Not Significant': 0,
+                        '< 10%': 1,
+                        '< 5%': 2,
+                        '< 2%' : 3,
+                        '< 1%' : 4,
+                        '< 0.5%' : 5
+                            }
+        if not suspect:
+            self.df = df.loc[df['classification2'] != 'suspect']
+        else:
+            self.df = df
+
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        filename = self.df.iloc[index]["file_jpg"]
+        md_inf_prob = self.prob[self.df.iloc[index][self.labeltype[0]]]
+        md_sup_prob = self.prob[self.df.iloc[index][self.labeltype[1]]]
+        image = PIL.Image.open(os.path.join(self.img_folder,filename))
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, md_inf_prob, md_sup_prob
+
+
+class RNFLDataLoader(torch.utils.data.Dataset):
+    def __init__(self, csv_path, img_folder, subjective_csv = '/home/vip/sayan-mandal/datasets/obj_criteria/20200623-images_with_subjective.csv', \
+         labeltype = 'rnflclass_g',suspect =False,transform = None):
+        df = pd.read_csv(csv_path, low_memory=False)
+        df_subjective = pd.read_csv(subjective_csv)
+        df_subjective = df_subjective[['maskedid']].drop_duplicates().reset_index(drop = True)
+        df_subjective['with_subjective'] = 1
+
+        df = df.join(df_subjective.set_index('maskedid'), on = 'maskedid')
+        df = df.loc[df.with_subjective.isnull() == True].reset_index(drop = True)
+        self.img_folder = img_folder
+        self.transform = transform
+        self.labeltype = labeltype    
+        self.prob = {'WNL': 0,
+                    'BL': 1,
+                    'ONL': 2
+                    }
+        if not suspect:
+            self.df = df.loc[df['classification2'] != 'suspect']
+        else:
+            self.df = df
+
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        filename = self.df.iloc[index]["file_jpg"]
+        rnfl = self.prob[self.df.iloc[index][self.labeltype]]
+        image = PIL.Image.open(os.path.join(self.img_folder,filename))
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, rnfl
+
+    
+class RNFLallDataLoader(torch.utils.data.Dataset):
+    def __init__(self, csv_path, img_folder, subjective_csv = '/home/vip/sayan-mandal/datasets/obj_criteria/20200623-images_with_subjective.csv', \
+         labeltype = ['rnflclass_ti','rnflclass_ni','rnflclass_ts','rnflclass_ns'],suspect =False,transform = None):
+        df = pd.read_csv(csv_path, low_memory=False)
+        df_subjective = pd.read_csv(subjective_csv)
+        df_subjective = df_subjective[['maskedid']].drop_duplicates().reset_index(drop = True)
+        df_subjective['with_subjective'] = 1
+
+        df = df.join(df_subjective.set_index('maskedid'), on = 'maskedid')
+        df = df.loc[df.with_subjective.isnull() == True].reset_index(drop = True)
+        self.img_folder = img_folder
+        self.transform = transform
+        self.labeltype = labeltype    
+        self.prob = {'WNL': 0,
+                    'BL': 1,
+                    'ONL': 2
+                    }
+        if not suspect:
+            self.df = df.loc[df['classification2'] != 'suspect']
+        else:
+            self.df = df
+
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        filename = self.df.iloc[index]["file_jpg"]
+        rnflclass_ti = self.prob[self.df.iloc[index][self.labeltype[0]]]
+        rnflclass_ni = self.prob[self.df.iloc[index][self.labeltype[1]]]
+        rnflclass_ts = self.prob[self.df.iloc[index][self.labeltype[2]]]
+        rnflclass_ns = self.prob[self.df.iloc[index][self.labeltype[3]]]
+        image = PIL.Image.open(os.path.join(self.img_folder,filename))
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, rnflclass_ti,rnflclass_ni,rnflclass_ts,rnflclass_ns
--- model.py
+++ model.py
@@ -95,3 +95,93 @@ class VAE(nn.Module):
         z,mean, logvar = self.encode(x)
         out = self.decode(z)
         return out, mean, logvar
+
+
+
+
+class convVAE(nn.Module):
+    def __init__(self, in_shape, n_latent):
+        super(convVAE, self).__init__()
+        self.in_shape = in_shape
+        self.lin_shape = 16
+        self.n_latent = n_latent
+        c,h,w = in_shape
+        self.z_dim = h//2**2 # receptive field downsampled 3 times
+        self.encoder = nn.Sequential(
+            nn.Conv2d(c, 16, kernel_size=3, stride=1, padding=1),  # 16, 128, 128
+            nn.BatchNorm2d(16),
+            nn.LeakyReLU(),
+            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),  # 32, 64, 64
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # 64, 64, 64
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(),
+            nn.Conv2d(64, self.lin_shape, kernel_size=3, stride=2, padding=1),  # 16, 32, 32
+            nn.BatchNorm2d(self.lin_shape),
+            nn.LeakyReLU(),
+            nn.Flatten()
+        )
+        self.fc1 = nn.Sequential(
+            nn.Linear(self.lin_shape * (self.z_dim)**2, n_latent*2),
+            nn.BatchNorm1d(n_latent*2),
+            nn.LeakyReLU(),
+            nn.Linear( n_latent*2,n_latent),
+            nn.BatchNorm1d(n_latent),
+            nn.LeakyReLU(),
+        )
+        self.fc21 = nn.Sequential(
+            nn.Linear(n_latent, n_latent),
+        )
+        self.fc22 = nn.Sequential(
+            nn.Linear(n_latent, n_latent),
+        )
+
+        self.fc3 = nn.Sequential(
+            nn.Linear(n_latent, n_latent*2),
+            nn.BatchNorm1d(n_latent*2),
+            nn.LeakyReLU(),
+            nn.Linear(n_latent*2, self.lin_shape * (self.z_dim)**2),
+            nn.BatchNorm1d(self.lin_shape * (self.z_dim)**2),
+            nn.LeakyReLU(),
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(self.lin_shape, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(16),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(16, 3, kernel_size=3, stride=1, padding=1),
+            nn.Sigmoid()
+        )
+
+    def sample_z(self, mean, logvar):
+        stddev = logvar.mul(0.5).cuda()
+        noise = Variable(torch.randn(*mean.size())).cuda()
+        return (noise * stddev) + mean
+
+    def bottleneck(self, h):
+        h = self.fc1(h)
+        mean, logvar = self.fc21(h), self.fc22(h)
+        z = self.sample_z(mean, logvar)
+        return z, mean, logvar
+
+    def encode(self, x):
+        x = self.encoder(x)
+        z, mean, logvar = self.bottleneck(x)
+        return z, mean, logvar
+
+    def decode(self, z):
+        out = self.fc3(z)
+        out = out.view(out.shape[0], self.lin_shape, self.z_dim,self.z_dim)
+        out = self.decoder(out)
+        return out
+
+    def forward(self, x):
+        z,mean, logvar = self.encode(x)
+        out = self.decode(z)
+        return out, mean, logvar
